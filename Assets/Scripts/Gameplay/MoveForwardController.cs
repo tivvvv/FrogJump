2f0d1fdd9892657857a8cd1e4662c15463742699
tivvvv <18612553935@163.com>
2025-06-03 22:46:55 +0800

    feat: 汽车出界自动销毁.

--- Assets/Scripts/Gameplay/MoveForwardController.cs
+++ Assets/Scripts/Gameplay/MoveForwardController.cs
@@ -4,9 +4,22 @@ public class MoveForwardController : MonoBehaviour
 {
     public float speed;
 
+    public float maxLength = 25;
+
+    private Vector2 startPos;
+
+    private void Start()
+    {
+        startPos = transform.position;
+    }
+
     private void Update()
     {
         Move();
+        if (Mathf.Abs(transform.position.x - startPos.x) > maxLength)
+        {
+            Destroy(this.gameObject);
+        }
     }
 
     private void Move()
